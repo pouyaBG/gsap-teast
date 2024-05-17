e9028354a06d5d1e56e20d2072de019d870988ad
Pouys Barari Gelyard <b_pooya@yahoo.com>
2024-05-17 18:59:27 +0330

    chg

--- components/TextScrool.jsx
+++ components/TextScrool.jsx
@@ -3,23 +3,27 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useEffect } from "react";
 import React from "react";
+
 gsap.registerPlugin(ScrollTrigger);
-const TextScrool = () => {
+
+const TextScroll = () => {
   useEffect(() => {
     const textWrappers = document.querySelectorAll(".your-text-wrapper-class");
+    const textone = document.querySelectorAll(".textone");
+    const texttwo = document.querySelectorAll(".texttwo");
+    const textthree = document.querySelectorAll(".textthree");
 
     const pinTrigger = ScrollTrigger.create({
       trigger: ".content-wrapper",
       pin: true,
       start: "top top",
-      end: "+=250%",
+      end: "+=350%",
     });
 
     let tl = gsap.timeline();
 
     tl.to(textWrappers, {
       x: "-200%",
-      opacity: 0,
       scrollTrigger: {
         start: () => pinTrigger?.start,
         end: () => pinTrigger?.end,
@@ -27,28 +31,67 @@ const TextScrool = () => {
       },
     });
 
+    // Add a ScrollTrigger for textone that only triggers once
+    ScrollTrigger.create({
+      trigger: ".endyou",
+      start: "top top", // Adjust start value to where you want the animation to start
+      onEnter: () => {
+        gsap.to(textone, {
+          scale: 1,
+          duration: 1,
+        });
+        gsap.to(texttwo, { opacity: 1, duration: 1, delay: 1 });
+      },
+    });
+    ScrollTrigger.create({
+      trigger: ".textthree",
+      start: "top top", // Adjust start value to where you want the animation to start
+      onEnter: () => {
+        gsap.to(textthree, {
+          opacity: 1,
+          duration: 0.2,
+          delay: 2,
+        });
+        gsap.to(textthree, {
+          rotationZ: 360,
+          duration: 0.5,
+          delay: 3,
+        });
+      },
+    });
+
     return () => {
       pinTrigger?.kill();
       tl.kill();
     };
-  });
+  }, []);
+
   return (
-    <div className="flex items-center">
-      <div>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis
-        minus voluptate quisquam molestiae similique explicabo debitis expedita,
-        distinctio ipsa suscipit culpa aut sint architecto molestias sit velit
-        unde doloribus quos.
-      </div>
-      <div className="content-wrapper overflow-x-hidden ">
-        <div className="your-text-wrapper-class flex justify-center items-center h-screen">
-          <h1 className="text-vw font-bold text-white whitespace-nowrap text-wrapper transform translate-x-[70%]">
-            this is a test gasp and may name is pouya
+    <div className="content-wrapper overflow-x-hidden">
+      <div className="your-text-wrapper-class flex justify-center items-center h-screen">
+        <div className="flex items-center transform translate-x-[70%] endyou">
+          <h1 className="text-vw font-bold text-white whitespace-nowrap text-wrapper">
+            this is a test gasp
           </h1>
+          <div className="mx-5 maintext flex items-center">
+            <div className="textone scale-0">
+              <h1 className="text-vw font-bold text-white">and</h1>
+            </div>
+            <div className="texttwo opacity-0 mx-5">
+              <h1 className="text-vw font-bold text-white whitespace-nowrap">
+                my name is
+              </h1>
+            </div>
+            <div className="textthree opacity-0 rotate-90 mx-5">
+              <h1 className="text-vw font-bold text-white whitespace-nowrap">
+                pouya
+              </h1>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default TextScrool;
+export default TextScroll;
